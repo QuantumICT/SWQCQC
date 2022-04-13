bfed12e130faec48298769200afbe351cd52c0b7
Zhiqian Xu <xuzhiqian20z@ict.ac.cn>
2022-04-13 23:22:12 +0800

    remove

--- QuantumSpin/src/circuit/abstractdefs.jl
+++
@@ -1,8 +0,0 @@
-
-
-abstract type AbstractQuantumGate{N} end
-const AbstractOneBodyGate = AbstractQuantumGate{1}
-const AbstractTwoBodyGate = AbstractQuantumGate{2}
-const AbstractThreeBodyGate = AbstractQuantumGate{3}
-
-abstract type AbstractQuantumCircuit end
--- QuantumSpin/src/mps/abstractdefs.jl
+++
@@ -1,5 +0,0 @@
-
-const MPSTensor{T} = AbstractArray{T, 3} where {T<:Number}
-const SingularVector{T} = AbstractVector{T} where {T <: Real}
-
-abstract type AbstractMPS end
